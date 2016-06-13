2806a0ffd488596328e595baedd26937b0af702e
kolialike <kolialike@gmail.com>
2016-06-13 14:27:27 +0300

    ofice-commite

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,34 @@
 jQuery(function($){
+	// tabs
+	var tabs = $(".tabs").find("a");
+	  var tabsContent = $(".tabs-content");
+	  tabs.on('click', function(event) {
+	    event.preventDefault();
+	    $(this).addClass('active').siblings().removeClass('active');
+	    var index = tabs.index($(this));
+	    tabsContent.eq(index).addClass('active').siblings().removeClass('active');    
+	  });
+
 	var companyServicesSelect = $("#company-services-select");
-	companyServicesSelect.on('change', function () {
-		console.log(event);
-		// attr({});
-		var optVal = companyServicesSelect.target("option").tagName.val();
-		$('"company-services-select-"+optVal').addClass('open').siblings().removeClass('open');
+	companyServicesSelect.on('change', function (event) {
+		var optVal = $(this).val();
+		$("#company-services-select-"+optVal).addClass('open').siblings().removeClass('open');
 	});
+
+	// language
+	var languageRu = $(".language-ru");
+	var language = $(".language")
+	languageRu.on('click', function(event) {
+		if (event.target.className == "language-ru") {
+	      language.toggleClass('language-open');
+	      event.preventDefault();
+	    };
+	});
+
+	 $('a[href^="#"]').click(function () { 
+     elementClick = $(this).attr("href");
+     destination = $(elementClick).offset().top;
+       $('body').animate( { scrollTop: destination }, 500 );
+     return false;
+   });
 });
